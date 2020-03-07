ef121c5943bb36af4aeea249e22e496b09e49180
Amogh Rao <amoghrao2003@gmail.com>
2020-03-06 23:47:37 -0800

    Add files via upload

---
+++ src/main/java/com/voldybot/haftamoney/HaftaMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.voldybot.haftamoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaftaMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaftaMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voldybot/haftamoney/HaftaMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voldybot.haftamoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaftaMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
